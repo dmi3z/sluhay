5c988a9e5acaf987c10bffd5b364edd4517b6a2d
StepankovAS <artur.stsepankou@innowise-group.com>
2021-06-22 10:28:10 +0300

    creating reward component

---
+++ src/app/modules/reward/reward.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reward',
+  templateUrl: './reward.component.html',
+  styleUrls: ['./reward.component.scss'],
+})
+export class RewardComponent implements OnInit {
+  constructor() {}
+
+  public ngOnInit(): void {}
+}
