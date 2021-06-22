55c3bbe486b7b0ce83bd2f766f53f23e3951fed8
StepankovAS <artur.stsepankou@innowise-group.com>
2021-06-22 10:28:01 +0300

    creating rotation component

---
+++ src/app/modules/rotation/rotation.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rotation',
+  templateUrl: './rotation.component.html',
+  styleUrls: ['./rotation.component.scss'],
+})
+export class RotationComponent implements OnInit {
+  constructor() {}
+
+  public ngOnInit(): void {}
+}
