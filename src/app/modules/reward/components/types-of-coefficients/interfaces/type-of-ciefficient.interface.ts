cc093a41fc29eeb5a5cdd33ec056a8c7839c939c
StepankovAS <artur.stsepankou@innowise-group.com>
2021-06-25 09:47:22 +0300

    types-of-coefficients page

---
+++ src/app/modules/reward/components/types-of-coefficients/interfaces/type-of-ciefficient.interface.ts
@@ -0,0 +1,6 @@
+export interface TypeOfCoefficient {
+  img: string;
+  name: string;
+  description: string;
+  orientation: string;
+}
--- src/app/modules/reward/components/types-of-coefficients/types-of-coefficients.component.ts
+++ src/app/modules/reward/components/types-of-coefficients/types-of-coefficients.component.ts
@@ -1,4 +1,7 @@
+import { TYPES_OF_COEFFICIENTS } from './constants/types-of-coefficients.constant';
+import { TypeOfCoefficient } from './interfaces/type-of-ciefficient.interface';
 import { Component } from '@angular/core';
+import { AGREEMENT_FOR_COEFFICIENTS } from './constants/agreement-for-coefficients.constant';
 
 @Component({
   selector: 'app-types-of-coefficients',
@@ -6,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./types-of-coefficients.component.scss'],
 })
 export class TypesOfCoefficientsComponent {
+  public typesOfCoefficients: TypeOfCoefficient[] = TYPES_OF_COEFFICIENTS;
+  public quoteText: string = AGREEMENT_FOR_COEFFICIENTS;
   constructor() {}
 }
