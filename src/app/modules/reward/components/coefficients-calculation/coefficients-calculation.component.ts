e48f0d7e70e46bef42d486357de83e6a2ad5af70
StepankovAS <artur.stsepankou@innowise-group.com>
2021-06-25 09:48:52 +0300

    coefficients-calculation component

---
+++ src/app/modules/reward/components/coefficients-calculation/coefficients-calculation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coefficients-calculation',
+  templateUrl: './coefficients-calculation.component.html',
+  styleUrls: ['./coefficients-calculation.component.scss'],
+})
+export class CoefficientsCalculationComponent {
+  constructor() {}
+}
